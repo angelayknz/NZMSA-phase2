9e5667bb8d1b06482a1bc0ef19e44569202e342e
Angela Yang <kyan211@mywhitecliffe.com>
2022-07-26 19:55:35 +1200

    complete step 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Pokemon Search </h1>
+
+      <div>
+        <label>Pokemon Name</label> <br />
+        <input
+          type="text"
+          id="pokemon-name"
+          name="pokemon-name"
+          onChange={(e) => setPokemonName(e.target.value)}
+        />{' '}
+        <br />
+        <button onClick={search}>Search</button>
+      </div>
+
+      <p>You have entered {pokemonName}</p>
     </div>
-  );
+  )
+
+  function search() {
+    alert('Search button has been clicked!')
+  }
 }
 
-export default App;
+export default App
